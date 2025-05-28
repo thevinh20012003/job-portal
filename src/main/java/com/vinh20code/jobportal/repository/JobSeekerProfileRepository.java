d6013d6721ca7b3c10648fe0bd233e43750ba4d7
VinhTThe <VinhTThe176288@fpt.edu.vn>
2025-05-29 00:54:10 +0700

    add support for Skills to Job Seekers

---
+++ src/main/java/com/vinh20code/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,11 @@
+package com.vinh20code.jobportal.repository;
+
+import com.vinh20code.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+
+    // Custom query methods can be added here if needed
+    // For example, to find a job seeker profile by user account ID:
+    // Optional<JobSeekerProfile> findByUserAccountId(int userAccountId);
+}
--- src/main/java/com/vinh20code/jobportal/service/UsersService.java
+++ src/main/java/com/vinh20code/jobportal/service/UsersService.java
@@ -1,6 +1,10 @@
 package com.vinh20code.jobportal.service;
 
+import com.vinh20code.jobportal.entity.JobSeekerProfile;
+import com.vinh20code.jobportal.entity.RecruiterProfile;
 import com.vinh20code.jobportal.entity.Users;
+import com.vinh20code.jobportal.repository.JobSeekerProfileRepository;
+import com.vinh20code.jobportal.repository.RecruiterProfileRepository;
 import com.vinh20code.jobportal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,14 +15,27 @@ import java.util.Optional;
 @Service
 public class UsersService {
     private final UserRepository usersRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
     @Autowired
-    public UsersService(UserRepository usersRepository) {
+    public UsersService(UserRepository usersRepository,
+                        JobSeekerProfileRepository jobSeekerProfileRepository,
+                        RecruiterProfileRepository recruiterProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.recruiterProfileRepository = recruiterProfileRepository;
         this.usersRepository = usersRepository;
     }
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return usersRepository.save(users);
+        Users savedUser = usersRepository.save(users);
+        int userTypeId = users.getUserTypeId().getUserTypeId();
+        if (userTypeId == 1) {
+            recruiterProfileRepository.save(new RecruiterProfile(savedUser));
+        } else {
+            jobSeekerProfileRepository.save(new JobSeekerProfile(savedUser));
+        }
+        return savedUser;
     }
 
     public Optional<Users> getUserByEmail(String email) {
